9bdba29375883b12a27a198dab2913c97fb30612
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-02 09:47:04 +0330

    commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ store.subscribe(()=>{
     console.log('state changed!')
 })
 
+// by using redux-toolkit the argument that we pass to the bugAdded is the payload so we want our payload
 store.dispatch(actions.bugAdded('bug1'))
 store.dispatch(actions.bugAdded('bug2'))
 store.dispatch(actions.bugAdded('bug3'))
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,16 @@
 
 // instead of these imports at above we :
 import { configureStore } from '@reduxjs/toolkit'
-
-
 import reducer from './bugs'
+
+
+
+// we can remove this :
+
+//# export default function configureStore(){
+//#     const store = createStore(reducer ,devToolsEnhancer({trace: true}))
+//#     return store;
+//# }
 
 
 //$ make sure to not have naming problems . you can change the name or just make it anonymous function

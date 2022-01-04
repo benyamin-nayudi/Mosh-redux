5e8533cd6be9b933631d1b9f170da02a7c52d3d7
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-04 10:44:46 +0330

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import { bugAdded } from "./store/bugs";
 import configureStore from "./store/configureStore";
 
+
 const store = configureStore();
-// store.dispatch(() =>{})
-// let dispatch a function :
 
 store.dispatch((dispatch , getState )=>{
-    // call an api
-    // when the promise is resolved => dispatch
    dispatch({type: 'bugReceived' , bugs: [1,2,3]})
    console.log(getState())
-    // when the promise is rejected => dispatch
 })
 
-store.dispatch(bugAdded({description: 'bug 1'}))
+store.dispatch({
+        type: 'error',
+        payload: {message : 'An error occured.'}
+    })
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,26 +2,17 @@
 import { configureStore  } from '@reduxjs/toolkit'
 import reducer from './reducer'
 import logger from './middleware/logger'
+import toast from './middleware/toast'
 
 
-//# we no longer need it
-//# import func from './middleware/func'
 
-//# export default function configureAppStore(){
-//#    return configureStore({ 
-//#         reducer , 
-//#         middleware:[logger({destination: 'console'}) , func] 
-//#     })    
-//# }
-
-
-// lets use thunk
 export default function configureAppStore(){
    return configureStore({ 
         reducer , 
         middleware: (getDefaultMiddleware) => [
             ...getDefaultMiddleware() , 
-            logger({destination: 'console'}) 
+            logger({destination: 'console'}) ,
+            toast
         ] 
     })    
 }

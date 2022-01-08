53fde0ef74d000290b98259cc54dace95985991a
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-08 08:34:17 +0330

    update

--- src/customStore.js
+++ src/customStore.js
@@ -1,4 +1,4 @@
-// here we are making a store . our store function returns and object with a property => state , that we have declared above it. if we import it in index.js we can modify it from outside of the function (and this is bad )
+// here we are making a custom store . our store function returns and object with a property => state , that we have declared above it. if we import it in index.js we can modify it from outside of the function (and this is bad )
 function createStore (){
     let state;
 
